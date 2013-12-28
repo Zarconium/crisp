6cf8813b6f581705885aa1a2d5f4b0fcaf15ca37
Zarconium <rjncruz@gmail.com>
2013-12-28 23:17:45 +0800

    Commit 12/28/2013 11:17 PM

--- application/models/test.php
+++ application/models/test.php
@@ -3,22 +3,44 @@ Class Test extends CI_Model
 {
 	function test()
 	{
-		/*$this->db->select('CONCAT_WS("", IF(LENGTH(student.Last_Name), student.Last_Name, NULL), ", ", IF(LENGTH(student.First_Name), student.First_Name, NULL), " ", IF(LENGTH(student.Middle_Initial), student.Middle_Initial, NULL), ". ", IF(LENGTH(student.Name_Suffix), student.Name_Suffix, NULL)) as Full_Name, school.name as School_Name, student.Last_Name', false);
-		$this->db->from('student');
-		$this->db->join('school', 'student.School_ID = school.School_ID');
+		$this->db->query('DROP TEMPORARY TABLE IF EXISTS temp1, temp2;');
 
-		$query = $this->db->get();*/
+		$this->db->query('CREATE TEMPORARY TABLE temp1 AS (
+			select class.Name as "Class Name", COUNT(DISTINCT Student_Class.Student_ID) as "Number of Students"
+			from Student_Class, Student, Class, subject, school, other_class, teacher
+			where Student_Class.student_id = student.Student_ID
+			and class.Class_ID = student_class.Class_ID
+			and other_class.Class_ID = class.Class_ID
+			and other_class.Teacher_ID = teacher.Teacher_ID
+			and subject.subject_id = class.Subject_ID
+			and school.School_ID = class.School_ID
+			and subject.Subject_Code = @subject
+			group by class.Name);');
 		
-		$this->db->query('SELECT Class.Name FROM Class');
+		$this->db->query('CREATE TEMPORARY TABLE temp2 AS (
+			select count(distinct class.Name), count(DISTINCT Student_Class.Student_ID) as "Number of Students"
+			from Student_Class, Student, Class, subject, school, other_class, teacher
+			where Student_Class.student_id = student.Student_ID
+			and class.Class_ID = student_class.Class_ID
+			and other_class.Class_ID = class.Class_ID
+			and other_class.Teacher_ID = teacher.Teacher_ID
+			and subject.subject_id = class.Subject_ID
+			and school.School_ID = class.School_ID
+			and subject.Subject_Code = @subject);');
+		
+		$this->db->query('select * from temp1 union select * from temp2;');
+		$this->db->query('DROP TEMPORARY TABLE IF EXISTS temp1, temp2;');
 
-		/*if($query->num_rows() > 0)
-		{
-			return $query->result();
-		}
-		else
-		{
-			return false;
-		}*/
+		/*$this->db->query('SET @startdate = "2011-01-01";');
+		$this->db->query('SET @enddate = "2012-12-31";');
+		$this->db->query('INSERT INTO Student_Taken_GCAT_List
+			SELECT st.Student_ID, st.School_ID, st.Gender
+			FROM Student as st , GCAT_Student as gs , Tracker as t , Student_Tracker as stt
+			WHERE stt.Student_ID = st.Student_ID
+			AND gs.Tracker_ID = stt.Tracker_ID
+			AND gs.Tracker_ID = t.Tracker_ID
+			AND t.Created_At
+			BETWEEN @startdate AND @enddate;');*/
 	}
 }
 ?>
