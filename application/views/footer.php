457591fe593a1b980cc501b369ce193d4b0605bd
RJ <rjncruz@gmail.com>
2014-01-02 18:48:09 +0800

    Minor changes
    Weird lines found in footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,17 +1,9 @@
-			</div>	
-		</div>	
-
+			</div>
+		</div>
 	<div id="footer">
 	BPAP
 	</div>
-</div>	
-<<<<<<< HEAD
-<script type="text/javascript" src="<?php echo base_url('js/bootstrap.js'); ?>"></script>	
-=======
-
-<script type="text/javascript" src="<?php echo base_url('js/jquery.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('js/bootstrap.js'); ?>"></script>	
-
->>>>>>> 96ad1a94cc752ac856fdb43eeaf93bbbb1d323b0
+</div>
+<script type="text/javascript" src="<?php echo base_url('js/bootstrap.js'); ?>"></script>
 </body>
 </HTML>
