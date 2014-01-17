624d64a0f10ac652ab9d5920ae0c85855a652ecd
Dayanara <dayanarasimon@yahoo.com>
2014-01-17 21:57:42 +0800

    More Changes

---
+++ application/views/forms/draft-form-program-adept-t3-tracker.php
@@ -0,0 +1,4 @@
+<div class="info-form">
+	<?php if (isset($draft_saved)) { echo '<div class="alert alert-info alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Draft saved.</div>';} ?>
+	
+</div>
