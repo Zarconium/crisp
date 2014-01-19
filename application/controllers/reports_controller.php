8f9796b8507e38e55e21a985e6afbe42ed6ed1d1
GLPU <glu_1214@yahoo.com.ph>
2014-01-18 20:47:31 -0800

    added suc report functions to controller

--- application/controllers/reports_controller.php
+++ application/controllers/reports_controller.php
@@ -60,6 +60,70 @@ class Reports_Controller extends CI_Controller {
 		$this->load->view('footer-print', $data);
 
 	}
+	
+	//Applies to all subjects (SMP,Gcat,Best,Adept)
+	function getAllStudentClassSUCReport()
+	{
+		$data['student_class_list'] = $this->report_suc->getStudentClass($subject,$school_code,$semester,$teacher_code);
+	
+		$this->load->view('reports/smp_student_class_suc_report', $data);
+	}
+
+	function getAllSMPStudentSUCReport()
+	{
+		$data['smp_student_list'] = $this->report_suc->getSMPStudent($subject,$school_code,$semester,$teacher_code,$class_name);
+	
+		$this->load->view('reports/smp_student_suc_report', $data);
+	}
+
+	function getAllGCATStudentSUCReport()
+	{
+		$data['gcat_student_list'] = $this->report_suc->getGCATStudent($subject,$school_code,$semester,$teacher_code,$class_name);
+	
+		$this->load->view('reports/suc_report_gcat_students', $data);
+	}
+
+	function getAllBestStudentSUCReport()
+	{
+		$data['best_student_list'] = $this->report_suc->getBestStudent($school_code,$date_start,$date_end);
+	
+		$this->load->view('reports/suc_report_best_students', $data);
+	}
+
+	function getAllAdeptStudentSUCReport()
+	{
+		$data['adept_student_list'] = $this->report_suc->getAdeptStudent($school_code,$date_start,$date_end);
+		
+		$this->load->view('reports/suc_report_adept_students', $data);
+	}
+
+	function getAllT3BestSUCReport()
+	{
+		$data['T3_best_list'] = $this->report_suc->getT3Best($school_code,$date_start,$date_end);
+	
+		$this->load->view('reports/suc_report_T3_Best', $data);
+	}
+
+	function getAllT3AdeptSUCReport()
+	{
+		$data['T3_adept_list'] = $this->report_suc->getT3Adept ($school_code,$date_start,$date_end);
+	
+		$this->load->view('reports/suc_report_T3_Adept', $data);
+	}
+
+	function getAllT3GCATSUCReport()
+	{
+		$data['T3_GCAT_list'] = $this->report_suc->getT3Gcat($school_code,$date_start,$date_end);
+	
+		$this->load->view('reports/suc_report_T3_GCAT', $data);
+	}
+
+	function getAllT3SMPSUCReport()
+	{
+		$data['SMP_total_list'] = $this->report_suc->getSMPTotal($school_code,$subject,$semester);
+	
+		$this->load->view('reports/suc_report_smp_total', $data);
+	}
 
 
 }
