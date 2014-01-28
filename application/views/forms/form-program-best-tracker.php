0c128e1da5328870042736e8e4bec5e5fa608a9e
franzfajardo22 <franz_fajardo22@yahoo.com>
2014-01-28 21:32:47 +0800

    Updated forms Program adept best and gcat

--- application/views/forms/form-program-best-tracker.php
+++ application/views/forms/form-program-best-tracker.php
@@ -1,7 +1,9 @@
 <div class="info-form">
 
 	<?php if (isset($draft_saved)) { echo '<div class="alert alert-info alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Draft saved.</div>';} ?>
-	
+	<?php if (isset($form_success)) { echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Student successfully added.</div>';} ?>
+	<?php if (isset($form_error)) { echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>There were errors in your input. Please check the fields and try again.</div>';} ?>
+
 	<h1>BEST Product Tracker Encoder</h1>
 	
 		<div class="save">
@@ -16,35 +18,40 @@
 		
 		<div class="form-group">		
 			<label>Last Name</label>			
-			<input class="form-control" type="text" id="lname">
+			<input class="form-control" type="text" name="last_name" value="<?php echo set_value('last_name'); ?>">
+				<?php echo form_error('last_name'); ?>
 		</div>
 		
 		<div class="form-group">
 			<label>First Name</label>
-			<input type="text" class="form-control" id="fname">
+			<input class="form-control" type="text" name="first_name" value="<?php echo set_value('first_name'); ?>">
+				<?php echo form_error('first_name'); ?>
 		</div>
 		
 		<div class="form-group">
 			<label>Middle Initial</label>
-			<input type="text" class="form-control" id="mname">
+			<input class="form-control" type="text" name="middle_initial" value="<?php echo set_value('middle_initial'); ?>">
+				<?php echo form_error('middle_initial'); ?>
 		</div>
-		
 		
 		<div class="form-group">
 			<label>Student Number</label><br/>
-			<input type="text" class="form-control" id="snumber">
+			<input class="form-control" type="text" name="student_number" value="<?php echo set_value('student_number'); ?>">
+				<?php echo form_error('student_number '); ?>
 		</div>
 		
 		<div class="form-group">
 			<label>Control Number</label><br/>
-			<input type="text" class="form-control" id="cnumber"> 
+			<input class="form-control" type="text" name="control_number" value="<?php echo set_value('control number'); ?>">
+				<?php echo form_error('control_number'); ?>
 		</div>		
 
 		<div class="form-group">
 			<label>Username</label>
-			<input type="text" class="form-control" id="username">
+			<input class="form-control" type="text" name="username value="<?php echo set_value('username'); ?>
+				<?php echo form_error('username'); ?>
 		</div>
-		
+
 		<div class="form-group">
 			<label>School</label>
 			<select class="form-control" name="school">
