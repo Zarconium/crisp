6f5f0d8272fe6941b416abfe494baaaaf7f44f32
Dayanara <dayanarasimon@yahoo.com>
2014-02-22 08:10:26 +0800

    UI

--- application/views/reports/program_report_suc_report.php
+++ application/views/reports/program_report_suc_report.php
@@ -1,7 +1,6 @@
 <h1>SUC Report</h1>
 <h4>School: <?php echo $school->Name; ?></h4>
 <h4>Period: <?php echo $start_date; ?> to <?php echo $end_date; ?> </h4>
-<legend>Teachers</legend>
 <div class="report-form">
 <table class="table table-striped table-bordered">
 <thead>
@@ -19,7 +18,7 @@
 </thead>
 <tbody>
 	<tr>
-		<td>Number of Teachers Trained</td>
+		<td>Teachers Trained</td>
 		<?php if($teacher_count_list) foreach ($teacher_count_list as $count): ?>
 			<td><?php echo $count->BPO101; ?></td>
 			<td><?php echo $count->BPO102; ?></td>
@@ -31,26 +30,8 @@
 			<td><?php echo $count->GCAT; ?></td>
 		<?php endforeach; ?>
 	</tr>
-</tbody>
-</table>
-
-<table class="table table-striped table-bordered">
-<thead>
-<tr>
-	<th>Description</th>
-	<th>BPO101</th>
-	<th>BPO102</th>
-	<th>Service Culture</th>
-	<th>Business Communication</th>
-	<th>Systems Thinking</th>
-	<th>AdEPT</th>
-	<th>BEST</th>
-	<th>GCAT</th>
-</tr>
-</thead>
-<tbody>
 	<tr>
-		<td>Number of Students Completed</td>
+		<td>Students Completed</td>
 		<?php if($student_completed_count_list) foreach ($student_completed_count_list as $count): ?>
 			<td><?php echo $count->BPO101; ?></td>
 			<td><?php echo $count->BPO102; ?></td>
@@ -62,26 +43,8 @@
 			<td><?php echo $count->GCAT; ?></td>
 		<?php endforeach; ?>
 	</tr>
-</tbody>
-</table>
-
-<table class="table table-striped table-bordered">
-<thead>
-<tr>
-	<th>Description</th>
-	<th>BPO101</th>
-	<th>BPO102</th>
-	<th>Service Culture</th>
-	<th>Business Communication</th>
-	<th>Systems Thinking</th>
-	<th>AdEPT</th>
-	<th>BEST</th>
-	<th>GCAT</th>
-</tr>
-</thead>
-<tbody>
 	<tr>
-		<td>Number of Students Currently Taking</td>
+		<td>Students Currently Taking</td>
 		<?php if($student_currently_taking_count_list) foreach ($student_currently_taking_count_list as $count): ?>
 			<td><?php echo $count->BPO101; ?></td>
 			<td><?php echo $count->BPO102; ?></td>
@@ -93,6 +56,6 @@
 			<td><?php echo $count->GCAT; ?></td>
 		<?php endforeach; ?>
 	</tr>
+	</tr>
 </tbody>
 </table>
-</div>
